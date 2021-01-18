8d5dce3de8d14a4468201192ff2e81787698b832
Firebladee <mgreenha@yahoo.co.uk>
2021-01-18 16:57:38 +0000

    Versions working

--- spec/rspec/dockerfile_spec.rb
+++ spec/rspec/dockerfile_spec.rb
@@ -5,7 +5,15 @@ require 'docker-api'
 
 describe 'Dockerfile' do
   image = Docker::Image.build_from_dir('.')
-  set :os, family: :redhat
+  set :os, family: :alpine
   set :backend, :docker
   set :docker_image, image.id
+
+  describe file('/docker-lib.sh') do
+    it { should exist }
+  end
+
+  describe package('ruby-dev') do
+    it { should be_installed }
+  end
 end
